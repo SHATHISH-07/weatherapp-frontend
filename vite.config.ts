9304c4bfc270cc27a55a9ae0ef2f666265365055
Shathish2004 <shathishkumaran07@gmail.com>
2025-05-11 16:33:59 +0530

    Adding to repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/weatherapp-frontend/",
   plugins: [react(), tailwindcss()],
 });
